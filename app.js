2c8a58a0dbf3e1e4c99391201cf0b0a980e1ae48
Jorge <jorgediazok@gmail.com>
2020-09-21 14:35:49 -0300

    math sequences

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-//Longest word challenge
+//LONGEST WORD CHALLENGE
 
 function longestWord(str) {
   let words = str.split(' ');
-  console.log(words);
+  //console.log(words);
   let longestWord = '';
 
   for (let word of words) {
@@ -12,8 +12,8 @@ function longestWord(str) {
   }
   return longestWord;
 }
-console.log(longestWord('I Woke up early today'));
-console.log(longestWord('I went straight to the beach'));
+//console.log(longestWord('I Woke up early today'));
+//console.log(longestWord('I went straight to the beach'));
 
 //Lo que sucede es que empieza el loop. Están todas las palabras separadas a causa
 //del str.split. Entonces tengo "I" "WOKE" "UP" "EARLY" "TODAY" por ejemplo.
@@ -23,3 +23,32 @@ console.log(longestWord('I went straight to the beach'));
 //la maquina es "I que tiene una letra, es mayor a longestword que no tiene ninguna?"
 //La respuesta es si, entonces longestword pasa a ser 1. Y asi sucesivamente hasta
 //Que encuentra la palabra maxima dentro de ese array y ya nadie la supera.
+
+//MATH SEQUENCES
+
+function mathSequences(arr) {
+  let arith = new Set();
+  let geo = new Set();
+
+  for (let i = 1; i < arr.length; i++) {
+    let number1 = arr[i] - arr[i - 1];
+    arith.add(number1);
+    let number2 = arr[i] / arr[i - 1];
+    geo.add(number2);
+  }
+  if (arith.size === 1) {
+    return 'Arithmetic';
+  }
+  if (geo.size === 1) {
+    return 'Geometric';
+  } else {
+    return -1;
+  }
+}
+
+console.log(mathSequences([2, 4, 6, 8]));
+//Arithmetic
+console.log(mathSequences([3, 9, 27]));
+//Geometric
+console.log(mathSequences([2, 5, 14, 89]));
+//-1
